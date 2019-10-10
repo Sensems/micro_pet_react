f3eb566a243f94436384a8e82ccdf4eb009cefed
sensems <11877660012@qq.com>
2019-10-10 18:29:37 +0800

    修改登录页

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Login from '../view/login/login'
+
+const routes = [
+	// {
+	// 	path:"/",
+	// 	component:Index,
+	// 	exact:true
+	// },
+	{
+		path:"/",
+		component:Login,
+		exact:true
+	},
+];
+
+export default routes
--- src/view/login/login.js
+++ src/view/login/login.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import './login.scss'
 
 class Login extends Component{
 	constructor(props) {
@@ -10,7 +13,24 @@ class Login extends Component{
 	}
 	render() {
 		return (
-				<div>这是登录页面</div>
+				<div className="loginBox">
+					<div className="loginData">
+						<div>
+							<TextField
+									label="用户名"
+							/>
+						</div>
+						<div>
+							<TextField
+									label="密码"
+									type="password"
+									autoComplete="current-password"
+							/>
+						</div>
+					</div>
+
+					<Button variant="contained" color="primary" size="small">登录</Button>
+				</div>
 		)
 	}
 }
